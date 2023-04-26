de75dd5019becea82b2da93f26acd39996d5ea8b
Filipe Silva <filipe.cavalcante0411@gmail.com>
2023-04-26 20:07:23 -0300

    js

---
+++ aulas/aula12/ex12.js
@@ -0,0 +1,10 @@
+var idade = 45
+
+console.log(`Você tem ${idade} anos`)
+if (idade < 16) {
+    console.log('Não pode votar')
+} else if (idade >= 18 && idade <= 65) {
+    console.log('Voto obrigatório')
+} else {
+    console.log('Voto facultativo')
+}
---
+++ aulas/aula12/ex14.js
@@ -0,0 +1,27 @@
+var date = new Date()
+var diaSem = date.getDay()
+
+console.log(diaSem)
+switch (diaSem) {
+    case 0:
+        console.log('Hoje é Domingo')
+        break
+    case 1:
+        console.log('Hoje é Segunda')
+        break
+    case 2:
+        console.log('Hoje é Terça')
+        break
+    case 3:
+        console.log('Hoje é Quarta')
+        break
+    case 4:
+        console.log('Hoje é Quinta')
+        break
+    case 5:
+        console.log('Hoje é Sexta')
+        break
+    case 6:
+        console.log('Hoje é Sábado')
+        break 
+}
