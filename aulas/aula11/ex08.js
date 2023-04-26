76a2383a711662f252f31269571ee376c1d321d2
Filipe Silva <filipe.cavalcante0411@gmail.com>
2023-04-25 22:48:13 -0300

    raiva

---
+++ aulas/aula11/ex08.js
@@ -0,0 +1,5 @@
+var velocidade = 70
+console.log(`Sua velocidade está em ${velocidade}Km/h.`)
+if (velocidade > 60) {
+    console.log('Você ultrapassou a velocidade permitida.')
+}
---
+++ aulas/aula11/ex09.js
@@ -0,0 +1,6 @@
+var origem = 'Brasil'
+if (origem == 'Brasil') {
+    console.log(`Você nasceu no ${origem}. Você é Brasileiro.`)
+} else {
+    console.log(`Você nasceu em ${origem}. Você é estrangeiro.`)
+}
