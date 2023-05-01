d7b96a4d3074ab049d2dc0930139d8a6bd5f8bb5
Filipe Silva <filipe.cavalcante0411@gmail.com>
2023-05-01 13:13:11 -0300

    finish

--- desafios/d03/script.js
+++ desafios/d03/script.js
@@ -1,12 +1,36 @@
 function contar() {
-    var c = document.getElementById('inicio').value
-    var fim = document.getElementById('fim').value
-    var passo = document.getElementById('passo').value
-    var resultado = document.getElementsByClassName('res')[0] 
-    
-    while (c <= fim) {
-        resultado.innerHTML = c
-        c = c + passo
+    var inicio = document.getElementById("inicio").value;
+    var fim = document.getElementById("fim").value;
+    var passo = document.getElementById("passo").value;
+    var resultado = document.getElementById("res");
+
+    if (inicio.length == 0 || fim.length == 0 || passo < 0) {
+        alert("[ERRO] Digite valores válidos!");
+    } else {
+        
+        var nInicio = Number(inicio);
+        var nFim = Number(fim);
+        var nPasso = Number(passo);
+
+        if (resultado.innerHTML != 0){
+            resultado.innerHTML = ''
+        }
+
+        if (passo.length == 0) {
+            nPasso = 1;
+            }
+
+        if (nInicio <= nFim) {
+
+            while (nInicio <= nFim) {
+                resultado.append(`${nInicio} `);
+                nInicio = nInicio + nPasso;
+            }
+        } else {
+
+            for (nInicio; nInicio >= nFim; nInicio -= nPasso) {
+                resultado.append(`${nInicio} `)
+            }
+        }
     }
-    
 }
