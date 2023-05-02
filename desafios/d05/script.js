47e95ea73dd6248bea1a0b4a599746d946b54a93
Filipe Silva <filipe.cavalcante0411@gmail.com>
2023-05-02 10:16:16 -0300

    tentando fazer

---
+++ desafios/d05/script.js
@@ -0,0 +1,11 @@
+var n = document.getElementById('numeros').value
+var num = Number(n)
+var listaNumeros = []
+
+function adicionar(num) {
+    listaNumeros.push(num)
+    alert(listaNumeros)
+}
+
+
+
