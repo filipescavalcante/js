859b6156bdea7ec6621ed9ca5c6bbf74d0c48296
Filipe Silva <filipe.cavalcante0411@gmail.com>
2023-05-06 20:52:18 -0300

    finish

--- desafios/d05/script.js
+++ desafios/d05/script.js
@@ -3,30 +3,70 @@ var res = document.getElementById('res')
 var analise = document.getElementById('analise')
 listaNumeros = []
 
+function numValido (num) {
+    if (num.length == 0 || num.value < 1 || num.value > 100) {
+        return false
+    } else {
+        return true
+    }
+}
+
 function adicionar() {
-    listaNumeros.push(Number(num.value))
-    let valor = document.createElement('option')
-    valor.innerHTML = `Você adicionou o valor ${num.value}`
-    res.append(valor)
-    num.value = ''
+    if (numValido (num) == false) {
+        alert ('[ERRO] Digite valores válidos!')
+    } else {
+        listaNumeros.push(Number(num.value))
+        let valor = document.createElement('option')
+        valor.innerHTML = `Você adicionou o valor ${num.value}`
+        res.append(valor)
+        num.value = ''
+        num.focus()
+        analise.innerHTML = ''
+    }
+}
+
+function resValido () {
+    if (listaNumeros.length == 0) {
+        return false
+    } else {
+        return true
+    }
 }
 
 function finalizar() {
-    listaNumeros.sort()
+    if (resValido () == false) {
+        alert ('[ERRO] Adicione valores!')
+    } else {
+        listaNumeros.sort()
 
-    let soma = 0
-    let c = 1
-    let v = listaNumeros.length
+        let total = listaNumeros.length
+        let maior = listaNumeros[0]
+        let menor = listaNumeros[0]
+        let soma = 0 
+        let media = 0
 
-    while (c <= v) {
-        soma = soma + listaNumeros[c] + listaNumeros[c-1]
-        c++
+        for (let pos in listaNumeros) {
+            if (listaNumeros[pos] > maior) {
+                maior = listaNumeros[pos]
+            }
+            if (listaNumeros[pos] < menor) {
+                menor = listaNumeros[pos]
+            }
+            soma += listaNumeros[pos]
+        }
+        media = soma / total
+
+        analise.innerHTML = `<p>A quantidade de elementos adicionados foi ${total} <br> 
+        O maior valor foi ${maior} <br>
+        O menor valor foi ${menor} <br>
+        A soma de todos os elementos foi de ${soma} <br>
+        A média dos elementos foi ${media.toFixed(2)}
+        </p>`
     }
+}
 
-    analise.innerHTML = `${soma}`
-
-    analise.innerHTML = `A quantidade de elementos adicionados foi ${listaNumeros.length} <br> 
-    O maior valor foi ${listaNumeros[listaNumeros.length -1]} <br>
-    O menor valor foi ${listaNumeros[0]} <br>
-    `
+function limpar() {
+    res.innerHTML = ''
+    analise.innerHTML = ''
+    listaNumeros = []
 }
