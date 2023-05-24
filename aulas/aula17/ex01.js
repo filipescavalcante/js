3980707c098ccc8d8858952b39236fa30e0df7d3
Filipe Silva <filipe.cavalcante0411@gmail.com>
2023-05-24 20:02:56 -0300

    map

--- aulas/aula17/ex01.js
+++
@@ -1,5 +0,0 @@
-let array = ['1', '2', '3']
-let int = (n) => parseInt(n)
-array.map((e) => {
-    return int(e)
-}) 
---
+++ aulas/aula17/map().js
@@ -0,0 +1,14 @@
+const numeros = [5, 2, 3, 1, 6]
+// const string = []
+
+// for (let n of numeros) {
+//     string.push(n.toString())
+// }
+
+// console.log(string)
+
+const str = numeros.map( n => n.toString() )
+
+console.log(numeros)
+console.log(str)
+
